76b4c6b4705feade450dbe6ab10fd071f9608b30
tobias bolivar <tobias.bolivar@litermi.com>
2022-11-30 15:43:55 -0400

    NWT-53: Fix SendGroupNotificationScheduler: renamed scheduler.

--- src/Schedulers/SendGroupNotificationScheduler.php
+++ src/Schedulers/SendGroupNotificationScheduler.php
@@ -18,13 +18,13 @@ class SendGroupNotificationScheduler extends Command
      *
      * @var string
      */
-    protected $signature = 'scheduler_project:error-notification-send-group';
+    protected $signature = 'scheduled_commands:error-notification-send-group';
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Process to send group notification, php artisan scheduler_project:error-notification-send-group';
+    protected $description = 'Process to send group notification, php artisan scheduled_commands:error-notification-send-group';
 
     /**
      * Create a new command instance.
